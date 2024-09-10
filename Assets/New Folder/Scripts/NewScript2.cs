d207de3552555219c3459d98c5ca9222603140b8
Yakovlev-Ilya-IT <67857830+wizard2122@users.noreply.github.com>
2024-09-10 18:47:45 +0300

    Add new script 2

---
+++ Assets/New Folder/Scripts/NewScript2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScript2 : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Add 2 script");
+    }
+
+}
